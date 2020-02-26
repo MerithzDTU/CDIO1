13518eff5b35deacf5d9a38f7d921fc9e86983b2
max <maxmagi@hotmail.com>
2020-02-26 14:27:13 +0100

    added ui package

---
+++ CDIO1/src/ui/IUI.java
@@ -0,0 +1,11 @@
+package ui;
+
+public interface IUI {
+    void menu();
+    void createUser();
+    void showUsers();
+    void showUser();
+    void editUser();
+    void deleteUser();
+    void exit();
+}
